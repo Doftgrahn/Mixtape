4a46df2646ccb35d8004ed00926b10f05965135a
Simon grahn <simon.grahn@gmail.com>
2019-12-27 21:21:38 +0100

    setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+
+const app = express()
+app.use(express.static(`${__dirname}/../../frontend/build/`))
+app.use(favicon(path.join(__dirname, '/../../frontend/build/favicon.ico')))
+
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+  res.json('hehe')
+})
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '../../frontend/build/')
+  })
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server listening on port ${port}`))
+
 console.log('hej vad händer?')
