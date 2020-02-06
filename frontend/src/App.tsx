bca2607d696012af03be8554758be74e820ed26e
Simon grahn <simon.grahn@gmail.com>
2020-02-06 12:55:16 +0100

    scroll to top on route changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useEffect, Suspense } from 'react'
 import './styles/App.scss'
-
 import { connect, useDispatch } from 'react-redux'
 import Routes from './routes/routes'
 import ReactGA from 'react-ga'
@@ -10,22 +9,19 @@ import { createBrowserHistory } from 'history'
 import { lightTheme, darkTheme } from './utils/colors/colors'
 import { getActiveUser } from './logic/auth/authAction'
 
-//Used for later?
-//import checkSpotifyToken from './utils/checkSpotifyToken/checkSpotifyToken'
-//import checkGoogleToken from './utils/checkGoogleToken/checkGoogleToken'
-
 import { AppInterface } from './types/propTypes'
 
 import Spinner from './components/shared/spinner/spinner'
+import ScrollToTop from './utils/scrollToTop/scrollToTop'
+
+const browserHistory = createBrowserHistory()
 
 ReactGA.initialize('UA-153619692-2')
-const browserHistory = createBrowserHistory()
 browserHistory.listen((location, _action) => {
   ReactGA.pageview(location.pathname + location.search)
 })
 
 const App: FC<AppInterface> = ({ theme, user }) => {
-  const { spotifyToken, googleToken } = user
   const dispatch = useDispatch()
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search)
@@ -43,22 +39,10 @@ const App: FC<AppInterface> = ({ theme, user }) => {
     })
   }, [theme])
 
-  useEffect(() => {
-    if (spotifyToken) {
-      //checkSpotifyToken(spotifyToken, dispatch)
-    }
-  }, [spotifyToken, dispatch])
-
-  useEffect(() => {
-    if (googleToken) {
-      // checkGoogleToken(googleToken, dispatch)
-      // Check if token from google is active.
-    }
-  })
-
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Suspense fallback={<Spinner />}>
           <Routes />
         </Suspense>
